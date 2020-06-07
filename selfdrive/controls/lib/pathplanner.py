13b56af44b4948add32564d7318a48b904da5426
multikyd <multikyd@gmail.com>
2020-06-07 15:36:42 +0900

    Update pathplanner.py

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -174,11 +174,11 @@ class PathPlanner():
 
       if self.lane_change_direction == LaneChangeDirection.left:
         torque_applied = sm['carState'].steeringTorque > 0 and sm['carState'].steeringPressed
-        if self.alc_nudge_less and 1.5 > self.pre_auto_LCA_timer > 1.0 and not lca_left:
+        if self.alc_nudge_less and 0.5 > self.pre_auto_LCA_timer > 0.1 and not lca_left:
           torque_applied = True # Enable auto LCA only once after 1 sec 
       else:
         torque_applied = sm['carState'].steeringTorque < 0 and sm['carState'].steeringPressed
-        if self.alc_nudge_less and 1.5 > self.pre_auto_LCA_timer > 1.0 and not lca_right:
+        if self.alc_nudge_less and 0.5 > self.pre_auto_LCA_timer > 0.1 and not lca_right:
           torque_applied = True # Enable auto LCA only once after 1 sec 
 
       lane_change_prob = self.LP.l_lane_change_prob + self.LP.r_lane_change_prob
